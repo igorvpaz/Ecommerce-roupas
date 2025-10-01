1f4eff2ec62dc753e1eed233746b08811816125c
Igor UiPath <igorvpaz@gmail.com>
2025-10-01 11:56:25 -0300

    Alterando nome da loja

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
         <header className="bg-white shadow-md">
             <div className="container mx-auto px-6 py-4 flex justify-between items-center">
                 <Link href="/" className="text-2xl font-bold text-gray-800">
-                    DevStore
+                    Fake Store
                 </Link>
 
                 <Navbar />
